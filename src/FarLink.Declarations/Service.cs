c6a2971321f9cfd92f54040909ce074c3919729e
IJSGaus <ijsgaus@gmail.com>
2018-02-05 00:03:31 +0300

    1

---
+++ src/FarLink.Declarations/Service.cs
@@ -0,0 +1,19 @@
+﻿namespace FarLink.Declarations
+{
+    public abstract class Service<T> 
+        where T : Service<T>, new()
+    {
+        protected Service(QualifiedIdentifier name)
+        {
+            Name = name;
+        }
+
+        public QualifiedIdentifier Name { get; }
+    }
+
+    public abstract class Event<TService> 
+        where TService : Service<TService>, new()
+    {
+        
+    }
+}
